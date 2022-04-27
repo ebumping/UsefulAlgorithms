4e69937c5a78544d50cf4df1261d709ee2129cf0
Djeffery <dylan.jeffery@snhu.edu>
2022-04-26 23:19:18 -0400

    Finished ContainsDuplicate

--- ContainsDuplicate/src/containsDuplicate/Solution.java
+++ ContainsDuplicate/src/containsDuplicate/Solution.java
@@ -1,5 +1,23 @@
 package containsDuplicate;
 
-public class Solution {
+import java.util.HashSet;
 
+public class Solution {
+	public boolean containsDuplicate(int[] nums) {
+		if(nums == null) {
+			throw new IllegalArgumentException("Input is null");
+		}
+		if(nums.length <= 1) {
+			return false;
+		}
+		
+		HashSet<Integer> found = new HashSet<>();
+		for(int i : nums) {
+			if(!found.add(i)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
 }
